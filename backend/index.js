3a22ada8c7f37b8206c806c3b75c970448568f24
Azharelahi <azharelahi321@gmail.com>
2025-04-26 00:11:03 +0500

    Cors issue settled , now the website is ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = "Mine Secret Key"; // Change this to something more secure in production
 const allowedOrigins = [
-  "https://canberra-express.vercel.app/",
+  "https://canberra-express.vercel.app",
   "http://localhost:4000",
 ];
+
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Add this if you're using cookies
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json());
 app.use(cookieParser()); // Use the cookie parser middleware
 
