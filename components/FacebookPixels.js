1db523a100f8b37dafbecc7427adcae7c9c1f558
Azharelahi <143085056+Azharelahi@users.noreply.github.com>
2026-01-03 00:20:32 +0500

    issues in first render removed

--- components/FacebookPixels.js
+++ components/FacebookPixels.js
@@ -1,31 +1,41 @@
-import Script from 'next/script';
-import { useEffect } from 'react';
+"use client"; // ⚠️ Required for any client-side hooks or scripts
+
+import Script from "next/script";
 
 const FacebookPixels = () => {
-  useEffect(() => {
-    if (!window.fbq) {
-      !(function(f,b,e,v,n,t,s){
-        if(f.fbq) return;
-        n=f.fbq=function(){ n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments) };
-        if(!f._fbq) f._fbq=n;
-        n.push=n;
-        n.loaded=!0;
-        n.version='2.0';
-        n.queue=[];
-        t=b.createElement(e); t.async=!0; t.src=v;
-        s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s);
-      })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+  return (
+    <>
+      {/* Facebook Pixel Base Code */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
 
-      window.fbq('init', process.env.NEXT_PUBLIC_FB_PIXEL_ID);
-      window.fbq('track', 'PageView');
-
-      window.fbTrackBooking = (amount) => {
-        window.fbq('track', 'Purchase', { value: amount, currency: 'AUD' });
-      };
-    }
-  }, []);
-
-  return null;
+      {/* NoScript fallback */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
+    </>
+  );
 };
 
 export default FacebookPixels;
